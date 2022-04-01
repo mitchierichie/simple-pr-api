f3c1acebe3154cfe33f3f531dd6d1e28497e0d39
mitchierichie <mitchell@nativerank.com>
2022-04-01 17:02:08 -0600

    int ReposService

---
+++ src/services/repos.service.ts
@@ -0,0 +1,8 @@
+// TODO: make model, define real interface/type
+export type Repo = Record<any, any>;
+
+const GITHUB_API_BASE_URL = 'https://api.github.com';
+
+class ReposService {}
+
+export default ReposService;
