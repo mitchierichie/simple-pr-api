dad40ee82479db215202464bdcfae6e3a1a9bb7e
mitchierichie <mitchell@nativerank.com>
2022-04-01 16:59:45 -0600

    register repos routes file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import App from '@/app';
-import IndexRoutes from '@/routes/index.routes';
+import IndexRoutes from '@routes/index.routes';
+import ReposRoutes from '@routes/repos.routes';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new IndexRoutes()]);
+const app = new App([new IndexRoutes(), new ReposRoutes()]);
 
 app.listen();
