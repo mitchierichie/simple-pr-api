1278da0d80c6f8ed829fbcef572f3a329f6810a9
mitchierichie <mitchell@nativerank.com>
2022-04-03 17:57:55 -0600

    handle cache full error

--- src/services/cache.service.ts
+++ src/services/cache.service.ts
@@ -1,16 +1,15 @@
 import NodeCache, { Key as NodeCacheKey } from 'node-cache';
 
-const secondsInTenMinutes = 600;
-const secondsInAnHour = 3600;
-const tenThousand = 10000;
-
-export type CacheKey = NodeCacheKey;
 declare type Key = NodeCacheKey;
+export type CacheKey = NodeCacheKey;
+
+const secondsInAnHour = 3600;
+const tenThousand = 2;
+const FULL_CACHE_ERROR = 'ECACHEFULL';
 
 class CacheService {
   private static nodeCache = new NodeCache({
     stdTTL: secondsInAnHour,
-    checkperiod: secondsInTenMinutes,
     maxKeys: tenThousand,
   });
 
@@ -19,7 +18,19 @@ class CacheService {
   }
 
   public static set<Type>(key: Key, value: Type) {
-    return this.nodeCache.set<Type>(key, value);
+    try {
+      return this.nodeCache.set<Type>(key, value);
+    } catch (error) {
+      if (error.name !== FULL_CACHE_ERROR) {
+        throw error;
+      }
+
+      const [firstKey] = this.nodeCache.keys();
+      const firstKeyValue = this.nodeCache.take(firstKey);
+      this.isKey(firstKeyValue) && this.nodeCache.has(firstKeyValue as Key) && this.nodeCache.del(firstKeyValue as Key);
+
+      return this.nodeCache.set<Type>(key, value);
+    }
   }
 
   public static get<Type>(key: Key) {
