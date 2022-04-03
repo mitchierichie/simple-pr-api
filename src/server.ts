d99ee986a7c7abd377dadddd6d9935ebec1dcbb9
mitchierichie <mitchell@nativerank.com>
2022-04-03 17:59:34 -0600

    register cache routes

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import App from '@/app';
 import IndexRoutes from '@routes/index.routes';
 import RepoRoutes from '@/routes/repo.routes';
 import validateEnv from '@utils/validateEnv';
+import CacheRoutes from './routes/cache.routes';
 
 validateEnv();
 
-const app = new App([new IndexRoutes(), new RepoRoutes()]);
+const app = new App([new IndexRoutes(), new RepoRoutes(), new CacheRoutes()]);
 
 app.listen();
