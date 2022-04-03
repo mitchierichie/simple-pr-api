e4632973abd7772e40bf443ed07dece59887b371
mitchierichie <mitchell@nativerank.com>
2022-04-03 14:03:52 -0600

    remove square brackets from incoming generic type

--- src/interfaces/https.interface.ts
+++ src/interfaces/https.interface.ts
@@ -4,6 +4,6 @@ export type IncomingRequestOptions = string | https.RequestOptions | URL;
 export type RequestOptions = string | https.RequestOptions;
 
 export interface CacheHitResponse<DataType = any> {
-  data: DataType[];
+  data: DataType;
   cacheHit: true;
 }
