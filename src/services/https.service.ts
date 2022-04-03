276aef304d59de5e17fc236c2db5e7822ada7e8c
mitchierichie <mitchell@nativerank.com>
2022-04-03 12:09:11 -0600

    refactor https service into OOP, mr clean!

--- src/services/https.service.ts
+++ src/services/https.service.ts
@@ -1,26 +1,50 @@
 import { IncomingMessage } from 'http';
 import https from 'https';
-import { URL } from 'url';
+import { Key } from 'node-cache';
+import { URL, urlToHttpOptions } from 'url';
 import CacheService from './cache.service';
-import RepoService from './repo.service';
 
-declare type RequestOptions = string | https.RequestOptions | URL;
+declare type IncomingRequestOptions = string | https.RequestOptions | URL;
+declare type RequestOptions = string | https.RequestOptions;
 
 class HttpsService {
+  private options: RequestOptions;
+  private response: IncomingMessage;
+  private resolve: (value: unknown) => void;
+  private reject: (reason?: any) => void;
   private body = [];
+  private path: string;
 
-  private errorCallback(error: Error, reject: { (reason?: any): void }) {
-    reject(error);
+  private initializeOptions = (options: IncomingRequestOptions) => {
+    if (typeof options === 'string') {
+      this.options = options;
+
+      return;
+    }
+
+    if ('path' in options) {
+      this.options = options;
+
+      return;
+    }
+
+    if ('href' in options) {
+      this.options = urlToHttpOptions(options);
+    }
+  };
+
+  private errorCallback(error: Error) {
+    this.reject(error);
   }
 
-  private dataCallback(response: IncomingMessage) {
-    response.on('data', data => {
+  private dataCallback() {
+    this.response.on('data', data => {
       this.body.push(data);
     });
   }
 
-  private endCallback(response: IncomingMessage, resolve: { (value: unknown): void; (arg0: any): void }) {
-    response.on('end', () => resolve(this.parseBody()));
+  private endCallback() {
+    this.response.on('end', () => this.resolve(this.parseBody()));
   }
 
   private parseBody() {
@@ -31,40 +55,73 @@ class HttpsService {
   }
 
   public async get(options: RequestOptions): Promise<any> {
-    return new Promise((resolve, reject) => {
-      // promisify the request
+    this.initializeOptions(options);
+    this.initializePath();
 
-      const request = https.get(options, (response: IncomingMessage) => {
-        this.dataCallback(response);
-        this.endCallback(response, resolve);
+    return new Promise(this.promisifyRequestCallback);
+  }
 
-        if (response.headers.etag) {
-          this.saveETag(options, response.headers.etag);
-        }
-      });
+  private promisifyRequestCallback(resolve: (value: unknown) => void, reject: (reason?: any) => void) {
+    this.resolve = resolve;
+    this.reject = reject;
+    // promisify the request
 
-      request.on('error', error => this.errorCallback(error, reject));
-      request.end();
+    const request = https.get(this.options, (response: IncomingMessage) => {
+      this.response = response;
+      const cacheKey = this.checkResponseCache();
+
+      if (CacheService.isKey(cacheKey)) {
+        resolve(this.returnCacheHitResponse(cacheKey as Key));
+
+        return;
+      }
+
+      this.addResponseCallbacks();
+      this.saveETag();
     });
+
+    request.on('error', error => this.errorCallback(error));
+    request.end();
   }
 
-  private saveETag(options: RequestOptions, etag: string) {
-    const path = this.normalizePath(options);
-    CacheService.addETag(path, etag);
+  private addResponseCallbacks() {
+    this.dataCallback();
+    this.endCallback();
   }
 
-  private normalizePath(options: RequestOptions) {
-    if (typeof options === 'string') {
-      return options;
+  private returnCacheHitResponse(cacheKey: Key) {
+    return {
+      data: CacheService.get(cacheKey),
+      cacheHit: true,
+    };
+  }
+
+  private checkResponseCache() {
+    if (this.response.statusCode !== 304) {
+      return false;
     }
 
-    if ('path' in options) {
-      return options.path;
+    if (typeof this.options === 'string') {
+      return false;
     }
 
-    if ('pathname' in options) {
-      return options.pathname;
+    if (!this.options.headers || !this.options.headers['If-None-Match']) {
+      return false;
     }
+
+    const key = this.options.headers['If-None-Match'];
+
+    return CacheService.isKey(key) && CacheService.has(key as Key) && key;
+  }
+
+  private saveETag() {
+    if (this.response.headers.etag) {
+      CacheService.set(this.path, this.response.headers.etag);
+    }
+  }
+
+  private initializePath() {
+    this.path = typeof this.options === 'string' ? this.options : this.options.path;
   }
 }
 
